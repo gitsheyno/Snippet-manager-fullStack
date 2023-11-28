4c11d92e967915bafa03b749498173e4a6575bb6
shayan heidary <hdrydeveloper@gmail.com>
2023-11-28 21:06:26 +0100

    useformstate

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -22,3 +22,32 @@ export async function deleteSnippet(id: number) {
 
   redirect(`/`);
 }
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  //   return { message: "Title Must be longer" };
+  //-------------------------------<< Validate Inputs >>-------------------------------
+  const title = formData.get("title") as string;
+  const code = formData.get("code") as string;
+
+  if (typeof title !== "string" || title.length < 3) {
+    return { message: "title must be longer" };
+  }
+  if (typeof code !== "string" || title.length < 10) {
+    return { message: "code must be longer" };
+  }
+  //-------------------------------<< Create New Record in DB >>-------------------------------
+  const snippet = await db.snipet.create({
+    data: {
+      title,
+      code,
+    },
+  });
+  console.log(snippet);
+
+  //-------------------------------<< Redirect to Root >>-------------------------------
+
+  redirect("/");
+}
